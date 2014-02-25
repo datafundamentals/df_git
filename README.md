22c78024587bdf1f998961973e14e599837dc352
jcarapet <jeffcarapetyan@gmail.com>
2014-02-25 15:52:39 -0600

    the most basic package known to man

---
+++ README.md
@@ -0,0 +1,13 @@
+# df_git cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: df_git
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+package "git" do
+	action :install
+end
